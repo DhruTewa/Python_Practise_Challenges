d9e6e3a697dc0086466ad814a1c5c7dd764362c7
Dhruv Tewari <dhrutewa@gmail.com>
2023-08-10 19:20:28 -0500

    Second Commit

---
+++ Challenge_1.py
@@ -0,0 +1,15 @@
+# This is the first program
+
+# Problem 1: Write a function that takes in an integer, minutes, and converts it into seconds
+
+def convert(minutes):
+    return minutes * 60
+
+def main__init__():
+    print(convert(5))
+    print(convert(3))
+    print(convert(2))
+
+# Problem 2: Write a function that takes two numbers as arguments and returns their sum.
+
+# Problem 3: Create a function that takes a string and returns the number (count) of vowels contained within it.
